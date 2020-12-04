fe9406ddf2f9c4adabd3ac84c0a8a1360b9d4324
Hadrien de Cuzey <hadrien.decuzey@kaikuhealth.com>
2020-12-04 08:00:47 +0200

    Update tests configs to also run tests against webpack 5

--- test/fixtures/g/webpack.config.js
+++ test/fixtures/g/webpack.config.js
@@ -3,12 +3,16 @@ const BugsnagSourceMapUploaderPlugin = require('../../../').BugsnagSourceMapUplo
 module.exports = {
   entry: './app.js',
   devtool: 'hidden-source-map',
-  output: {
-    path: __dirname,
-    filename: './bundle.js',
-    publicPath: 'https://foobar.com/js',
-    futureEmitAssets: true
-  },
+  output: Object.assign(
+    {
+      path: __dirname,
+      filename: './bundle.js',
+      publicPath: 'https://foobar.com/js',
+    },
+    // As per webpack documentation:
+    // "output.futureEmitAssets option will be removed in webpack v5.0.0 and this behaviour will become the new default."
+    // so it can safely be omitted for webpack>=5 while still getting a similar result
+    parseInt(process.env.WEBPACK_VERSION, 10) < 5 ? { futureEmitAssets: true } : {}),
   plugins: [
     new BugsnagSourceMapUploaderPlugin({
       apiKey: 'YOUR_API_KEY',
