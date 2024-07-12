1848a95c902d849d69e27aa9e25af2b68dd7b5c3
James Lindsay <james.lindsay@smartbear.com>
2024-07-12 10:49:26 +0100

    Fix CI for webpack 3

--- bin/ci-install.sh
+++ bin/ci-install.sh
@@ -1,5 +1,5 @@
 if [ "$WEBPACK_VERSION" -eq "3" ]; then
-  npm i --no-save --no-package-lock --legacy-peer-deps webpack@3 mini-css-extract-plugin@1
+  npm i --no-save --no-package-lock webpack@3
 elif [ "$WEBPACK_VERSION" -eq "4" ]; then
   npm i --no-save --no-package-lock webpack@4 webpack-cli@4 mini-css-extract-plugin@1 css-loader@5
 else
